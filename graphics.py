633f84cfb855073e43f0462ba5f86f2b1d782cd6
Surendhran-P <119148273+Surendhran-P@users.noreply.github.com>
2024-03-31 21:57:29 +0530

    Update graphics.py
    Defined init function in PlotTraj

--- graphics.py
+++ graphics.py
@@ -1,9 +1,37 @@
 class PlotTraj:
     def __init__(self,filename):
-        pass
+        with open(filename) as f:
+            self.data = f.read()
+        
+        self.data = self.data.split('\n\n')
+        self.masses = []
+        self.names = []
+        self.trajectories = []
+        sub_trajectories = []
+        
+        for heading in self.data:
+            part = heading.split('\n')
+            
+            if part[0] == 'NUM_BODIES':
+                self.num_bodies = int(part[1])
+            if part[0] == 'NUM_STEPS':
+                self.num_steps = int(part[1])
+            if part[0] == 'MASSES':
+                for i in range(self.num_bodies):
+                    self.masses.append(part[i+1]) 
+            if part[0] == 'NAMES':
+                for i in range(self.num_bodies):
+                    self.names.append(part[i+1])
+            if part[0] == 'TRAJECTORIES':
+                for i in range(self.num_steps):
+                    for j in range(2):
+                        sub_trajectories.append(part[j])
+                    self.trajectories.append(sub_trajectories)
 
-    def _update(self):
+    def _update(self, frame):
         pass
 
     def animate(self):
         pass
+
+obj = PlotTraj('trajectory.txt')
